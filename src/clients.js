ade769dc37c9adfefca77de1904c47a6054be3e5
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2023-12-19 22:15:22 +0700

    Update clients.js

--- src/clients.js
+++ src/clients.js
@@ -4,7 +4,11 @@ export let clients = [];
  *
  * @param {string} client_id
  * @param {object} client Client object from wwebjs
+ * @param {string|null} qr
+ * @param {boolean} auth
+ * @returns client object
  */
-export const add_client = (client_id, client, qr, auth) => {
+export const add_client = (client_id, client, qr = null, auth = false) => {
   clients[client_id] = { client, qr, auth };
+  return clients[client_id];
 };
